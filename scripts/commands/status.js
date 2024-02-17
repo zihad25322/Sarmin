10e3fe72830e284022017d1fd7baca34489200d9
Mahabubpk404 <160235221+Mahabubpk404@users.noreply.github.com>
2024-02-17 17:53:33 +0600

    Update status.js

--- scripts/commands/status.js
+++ scripts/commands/status.js
@@ -1,6 +1,6 @@
 /** Đổi Credit ? Bọn t đã không mã hóa cho mà edit rồi thì tôn trọng nhau tý đi ¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: `${global.config.PREFIX}`,
+  name: "status",
   version: "1.0.0", 
   permission: 0,
   credits: "nayan",
@@ -22,7 +22,7 @@ const fs = global.nodemodule["fs-extra"];
              ];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
-  "",
+  "https://i.imgur.com/bMz46lG.mp4",
 "",
 "",
 "",
@@ -46,7 +46,7 @@ const fs = global.nodemodule["fs-extra"];
 "",
 "",
 "",
-"https://i.imgur.com/bMz46lG.mp4"
+"https://i.imgur.com/bMz46lG.mp4",
 
 
   
