614f572d05dc67854cd37f1b04a5f04edfde39e1
Mahabubpk404 <160235221+Mahabubpk404@users.noreply.github.com>
2024-02-17 17:49:45 +0600

    Create status.js

---
+++ scripts/commands/status.js
@@ -0,0 +1,56 @@
+/** Đổi Credit ? Bọn t đã không mã hóa cho mà edit rồi thì tôn trọng nhau tý đi ¯\_(ツ)_/¯ **/
+module.exports.config = {
+  name: `${global.config.PREFIX}`,
+  version: "1.0.0", 
+  permission: 0,
+  credits: "nayan",
+  description: "", 
+  prefix: true,
+  category: "user",
+  usages: "",
+  cooldowns: 5, 
+  dependencies: {
+	}
+};
+
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["উপন‍্যাসের  উপসংহারে  যদি বিচ্ছেদ  লিখা থাকে\n\n'' তবে সুচনা কেনো এত রঙিন হয়''!! "
+             ];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = [
+  "",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"https://i.imgur.com/bMz46lG.mp4"
+
+
+  
+];
+	 var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
+   };
