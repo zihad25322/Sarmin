8cbc9c222d4232cabf68534968195013840e8c00
Mahabubpk404 <160235221+Mahabubpk404@users.noreply.github.com>
2024-02-24 07:34:42 +0600

    Update 💔.js

--- scripts/commands/💔.js
+++ scripts/commands/💔.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-	name: "sad0💔",
+	name: "sad026",
     version: "1.1.1",
 prefix: true,
 	permssion: 0,
@@ -14,13 +14,13 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	if (event.body.indexOf("💔")==0 || event.body.indexOf("💔")==0 || event.body.indexOf("💔")==0 || event.body.indexOf("💔")==0) {
 		var msg = {
-				body: "_বিজ্ঞান তুমি এখনো ব্যর্থ কারণ😌🖤...\nতুমি তৈরি করতে পারোনি স্মৃতি ভোলার যন্ত্র🙂💔🥺",
+				body: "-বিজ্ঞান তুমি এখনো ব্যর্থ কারণ😌🖤...\nতুমি তৈরি করতে পারোনি স্মৃতি ভোলার যন্ত্র🙂💔🥺 \nミM　Á　H　Â　B　Ü　 Rミ",
 				attachment: fs.createReadStream(__dirname + `/cache/received_1055260255533365.mp4`)
 			}
 			api.sendMessage( msg, threadID, messageID);
-    api.setMessageReaction("", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🫠", event.messageID, (err) => {}, true)
 		}
 	}
 	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-          
+}
