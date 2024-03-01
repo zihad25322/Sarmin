ef936a9166fbbae9a04be45b5ecb9c9dc884b78d
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-01 06:12:35 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -29,7 +29,6 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
 𝐍𝐚𝐦𝐞       : Mahabub Rahman
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/mdkajol.hossen.140
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : Islam
 𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: Bogura, Bangladesh
 𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: kundogram, adomdighi, Bogura
@@ -42,6 +41,6 @@ var callback = () => api.sendMessage({body:`
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/.......
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/mdkajol.hossen.140`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100014754734049/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
