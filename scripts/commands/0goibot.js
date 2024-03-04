4dfc98eeb9a05c1a295050a8101dd40a1128c9e4
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-04 15:43:10 +0600

    Update 0goibot.js

--- scripts/commands/0goibot.js
+++ scripts/commands/0goibot.js
@@ -1,16 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 
-module.exports.config = {
-	name: "sad009",
-    version: "1.1.1",
-prefix: true,
-	permssion: 0,
-	credits: "John Lester", 
-	description: "Just Respond",
-	category: "no prefix",
-    cooldowns: 5, 
-};
+
 
 module.exports.handleEvent = async function ({ api, event }) {
   if (!(event.body.indexOf("janu") === 0 || event.body.indexOf("Janu") === 0)) return;
