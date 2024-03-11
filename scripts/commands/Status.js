e44fab9f435368db349b9cefe65695b7b59535b0
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-11 12:52:56 +0600

    Update Status.js

--- scripts/commands/Status.js
+++ scripts/commands/Status.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["--Khan Rahul RK--"];
+   var hi = ["--VIDEO FROM MAHABUB RAHMAN--"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
