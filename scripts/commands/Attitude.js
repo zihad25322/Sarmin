db684728c7c6bc4d969dea43f6295fc947b3ee46
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-11 10:34:06 +0600

    Update Attitude.js

--- scripts/commands/Attitude.js
+++ scripts/commands/Attitude.js
@@ -1,18 +1,16 @@
-/** I am doing this coding with a lot of difficulty, please don't post it yourself¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "attitude",
-  version: "1.0.0",
-  permssion: 0,
+  name: "attitude", 
+  version: "1.0.0", 
+  permission: 0,
+  credits: "Nayan",
+  description: "example",
   prefix: true,
-  credits: "nayan",
-  description: "ATTITUDE VEDIO",
-  Category: "video",
-  usages: "user",
+  category: "Fun", 
+  usages: "user", 
   cooldowns: 5,
   dependencies: {
-    "request":"",
-    "fs-extra":"",
-    "axios":""
+        "axios": "",
+        "fs-extra": ""
   }
 };
 
