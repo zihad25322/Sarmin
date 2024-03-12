3033ab62a536dbbea49cd3a3739cd470b01a517a
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-12 14:54:04 +0600

    Update 0galagali.js

--- scripts/commands/0galagali.js
+++ scripts/commands/0galagali.js
@@ -1,14 +1,4 @@
-const fs = require("fs");
-module.exports.config = {
-	name: "gali356",
-    version: "1.1.1",
-prefix: true,
-	permssion: 0,
-	credits: "John Lester", 
-	description: "Just Respond",
-	category: "no prefix",
-    cooldowns: 5, 
-};
+
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
