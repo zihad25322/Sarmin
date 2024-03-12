3028e60ea05a1811fcd50bbdd1b29902be5e20ec
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-12 21:13:50 +0600

    Create Mahabub.js

---
+++ scripts/commands/Mahabub.js
@@ -0,0 +1,31 @@
+module.exports.config = {
+  name: "mahabub"",
+  name:"Mahabub", 
+  version: "1.0.0", 
+  permission: 0,
+  credits: "Imran Ahmed_X_Mahabub Rahman",
+  description: "example",
+  prefix: false,
+  category: "Fun", 
+  usages: "user", 
+  cooldowns: 5,
+  dependencies: {
+        "axios": "",
+        "fs-extra": ""
+  }
+};
+
+module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+   var hi = ["~HI..IM MAHABUB RAHMAN~"];
+  var know = hi[Math.floor(Math.random() * hi.length)];
+  var link = [
+
+  "https://drive.google.com/uc?id=1B31nfigyaYEyKhkDeRZNn5Nab8As6NjP",
+    "https://drive.google.com/uc?id1B6ZnpXOkwV1jqjhjGECv3i8Z4lsi0rLb"
+];
+     var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/15.mp4")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/15.mp4"));    
+      return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/15.mp4")).on("close",() => callback());
+   };
