b74b3b53c76839501e2e8962cc914ecacd82e89b
Github4111 <161781476+Github4111@users.noreply.github.com>
2024-03-16 12:40:05 +0600

    Update ig.js

--- scripts/commands/ig.js
+++ scripts/commands/ig.js
@@ -1,6 +1,6 @@
 /** Đổi Credit ? Bọn t đã không mã hóa cho mà edit rồi thì tôn trọng nhau tý đi ¯\_(ツ)_/¯ **/
 module.exports.config = {
-  name: "t",
+  name: `${global.config.PREFIX}`,
   version: "1.0.0", 
   permission: 0,
   credits: "nayan",
