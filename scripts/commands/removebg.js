dcbccb3a7d375583d27dec346aad4154625e5f06
OM E <161785462+OME06@users.noreply.github.com>
2024-05-05 13:52:32 +0600

    Update removebg.js

--- scripts/commands/removebg.js
+++ scripts/commands/removebg.js
@@ -1,26 +1,67 @@
-module.exports.config = {
-  name: "kickall",
-  version: "1.0.0",
-  permission: 2,
-  credits: "OME",
-  description: "Kick out all the member inside of the group.",
-  category: "group",
-  usages: "[]",
-  cooldowns: 3,
-  prefix: true
-};
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs-extra');
+const path = require('path');
+const {image} = require('image-downloader');
 
-module.exports.run = async function({ api, event, args }) {
-  var threadInfo = await api.getThreadInfo(event.threadID)
-  var id = threadInfo.participantIDs
-  const user = args.join(" ")
-  function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  };
-  for (let user of id) {
-    await delay(5000)
-    api.removeUserFromGroup(user, event.threadID, user);
+
+module.exports.run = async function({
+    api, event, args
+}){
+    try {
+      var tpk = `🖼️=== [ REMOVING BACKGROUND ] ===🖼️
+━━━━━━━━━━━━━━━
+[⚜️]➜ Api By MOHAMMAD NAYAN`;
+        if (event.type !== "message_reply") return api.sendMessage("[⚜️]➜ You must reply to a photo", event.threadID, event.messageID);
+        if (!event.messageReply.attachments || event.messageReply.attachments.length == 0) return api.sendMessage("[⚜️]➜ You must reply to a photo", event.threadID, event.messageID);
+        if (event.messageReply.attachments[0].type != "photo") return api.sendMessage("[⚜️]➜ This is not an image", event.threadID, event.messageID);
+
+        const content = (event.type == "message_reply") ? event.messageReply.attachments[0].url : args.join(" ");
+        const KeyApi = ["qReKoWSpkMAi2vbi6RUEHctA","ho37vvCUppqTKcyfjbLXnt4t","ytr2ukWQW2YrXV8dshPbA8cE"]
+        const inputPath = path.resolve(__dirname, 'cache', `photo.png`);
+         await image({
+        url: content, dest: inputPath
+    });
+        const formData = new FormData();
+        formData.append('size', 'auto');
+        formData.append('image_file', fs.createReadStream(inputPath), path.basename(inputPath));
+        axios({
+            method: 'post',
+            url: 'https://api.remove.bg/v1.0/removebg',
+            data: formData,
+            responseType: 'arraybuffer',
+            headers: {
+                ...formData.getHeaders(),
+                'X-Api-Key': KeyApi[Math.floor(Math.random() * KeyApi.length)],
+            },
+            encoding: null
+        })
+            .then((response) => {
+                if (response.status != 200) return console.error('Error:', response.status, response.statusText);
+                fs.writeFileSync(inputPath, response.data);
+                return api.sendMessage({body:tpk, attachment: fs.createReadStream(inputPath)},event.threadID, () => fs.unlinkSync(inputPath));
+            })
+            .catch((error) => {
+                return console.error('Request failed:', error);
+            });
+     } catch (e) {
+        console.log(e)
+        return api.sendMessage(`[⚜️]➜ Nayan Server Is Busy Now`, event.threadID, event.messageID);
   }
 };
 
-Remove emoji 😙💫💦 
+module.exports.config = {
+    name: "kick",
+    version: "1.0.0",
+    permission: 2,
+    credits: "OME",
+    description: "",
+    prefix: true,
+    category: "prefix",
+    usages: "reply",
+    cooldowns: 10,
+    dependencies: {
+       'form-data': '',
+       'image-downloader': ''
+    }
+}; 
