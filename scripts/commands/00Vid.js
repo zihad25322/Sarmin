90735dd11ce461a74ac21cff38ece330c855c945
zihad25322 <162394310+zihad25322@users.noreply.github.com>
2024-06-09 07:01:16 +0600

    Update 00Vid.js

--- scripts/commands/00Vid.js
+++ scripts/commands/00Vid.js
@@ -12,7 +12,7 @@ prefix: true,
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("💀")==0 || event.body.indexOf("😓")==0 || event.body.indexOf("😓")==0 || event.body.indexOf("😓")==0) {
+	if (event.body.indexOf("💀")==0 || event.body.indexOf("😅")==0 || event.body.indexOf("😓")==0 || event.body.indexOf(😔")==0) {
 		var msg = {
 				body: "-আলোর খোঁজ করতে গিয়ে- অন্ধকারে হারিয়ে গেছি আমি 😅💔..-!! 🙂 \nミO M E ミ",
 				attachment: fs.createReadStream(__dirname + `/cache/received_908287014131424.mp4`)
